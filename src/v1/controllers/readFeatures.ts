6cce84180af003a080f68d2a0ab9ee2013bbc3a4
xavigrausebastian <xavier.grau@kreatings.com>
2022-06-27 14:09:04 +0200

    ADD multiple endpoint functions

--- src/v1/controllers/readFeatures.ts
+++ src/v1/controllers/readFeatures.ts
@@ -2,11 +2,105 @@ import { Request, Response } from "express";
 import Feature from "../models/Feature";
 import { arrayToGeoJSON } from "../services/featuresService";
 
-export function getFeaturesByCity(req: Request, res: Response) {
+export function getFeatureById(req: Request, res: Response) {
 
-    Feature.find({ "properties.city": req.params.city }, function (err, results) {
+    Feature.findById(req.params.id, (err, results) => {
+
         if (err) {
-            return res.status(500).send( { message: `Error getting locations ${err.message}` });
+            return res.status(500).send( { message: `Error getting features ${err.message}` });
+        }
+        if (results.length === 0) {
+            return res.status(404).send( { message: 'No results found' });
+        }
+        res.status(200).send(results);
+    });
+}
+
+export function getFeatureNearPoint(req: Request, res: Response) {
+
+    const query = Feature.find({ "geometry.type": "Point" })
+        .where("geometry.coordinates")
+        .near({
+            center: {
+                type: 'Point',
+                coordinates: [req.params.lat, req.params.long]
+            },
+            // Converting meters to miles
+            maxDistance: parseFloat(req.params.meters) * 1609.34,
+            spherical: true
+        });
+
+    query.exec((err, results) => {
+        if (err) {
+            return res.status(500).send( { message: `Error getting features ${err.message}` });
+        }
+        if (results.length === 0) {
+            return res.status(404).send( { message: 'No results found' });
+        }
+        res.status(200).send(arrayToGeoJSON(results));
+    });
+}
+
+export function getFeaturesByTown(req: Request, res: Response) {
+
+    Feature.find({ "properties.town": req.params.town }, (err, results) => {
+        if (err) {
+            return res.status(500).send( { message: `Error getting features ${err.message}` });
+        }
+        if (results.length === 0) {
+            return res.status(404).send( { message: 'No results found' });
+        }
+        res.status(200).send(arrayToGeoJSON(results));
+    });
+}
+
+export function getFeaturesByPostalCode(req: Request, res: Response) {
+
+    Feature.find({ "properties.postal_code": req.params.postal_code }, (err, results) => {
+        if (err) {
+            return res.status(500).send( { message: `Error getting features ${err.message}` });
+        }
+        if (results.length === 0) {
+            return res.status(404).send( { message: 'No results found' });
+        }
+        res.status(200).send(arrayToGeoJSON(results));
+    });
+}
+
+export function getFeaturesByProvince(req: Request, res: Response) {
+
+    Feature.find({ "properties.province": req.params.province }, (err, results) => {
+
+        if (err) {
+            return res.status(500).send( { message: `Error getting features ${err.message}` });
+        }
+        if (results.length === 0) {
+            return res.status(404).send( { message: 'No results found' });
+        }
+        res.status(200).send(arrayToGeoJSON(results));
+    });
+}
+
+export function getFeaturesByComAut(req: Request, res: Response) {
+
+    Feature.find({ "properties.comAut": req.params.region }, (err, results) => {
+
+        if (err) {
+            return res.status(500).send( { message: `Error getting features ${err.message}` });
+        }
+        if (results.length === 0) {
+            return res.status(404).send( { message: 'No results found' });
+        }
+        res.status(200).send(arrayToGeoJSON(results));
+    });
+}
+
+export function getFeaturesByCountry(req: Request, res: Response) {
+
+    Feature.find({ "properties.country": req.params.country }, (err, results) => {
+
+        if (err) {
+            return res.status(500).send( { message: `Error getting features ${err.message}` });
         }
         if (results.length === 0) {
             return res.status(404).send( { message: 'No results found' });
@@ -17,9 +111,9 @@ export function getFeaturesByCity(req: Request, res: Response) {
 
 export function getAllFeatures(req: Request, res: Response) {
 
-    Feature.find({}, function (err, results) {
+    Feature.find({}, (err, results) => {
         if (err) {
-            return res.status(500).send( { message: `Error getting locations ${err.message}` });
+            return res.status(500).send( { message: `Error getting features ${err.message}` });
         }
         if (results.length === 0) {
             return res.status(404).send( { message: 'No results found' });
--- src/v1/routes/readFeatures.ts
+++ src/v1/routes/readFeatures.ts
@@ -3,11 +3,39 @@ import express from 'express';
 const router = express.Router();
 
 // * Import controller
-import { getFeaturesByCity, getAllFeatures } from "../controllers/readFeatures";
+import {
+    getFeatureById,
+    getFeaturesByPostalCode,
+    getFeaturesByTown,
+    getFeaturesByProvince,
+    getFeaturesByComAut,
+    getFeaturesByCountry,
+    getAllFeatures,
+    getFeatureNearPoint
+} from "../controllers/readFeatures";
 
-// * Endpoint used to get all the features from one location
+// * Get features by ID
+router.get('/id/:id', getFeatureById);
 
-router.get('/city/:city', getFeaturesByCity);
+// * Get features by postal code
+router.get('/code/:postal_code', getFeaturesByPostalCode);
+
+// * Get features by town
+router.get('/town/:town', getFeaturesByTown);
+
+// * Get features by province
+router.get('/province/:province', getFeaturesByProvince);
+
+// * Get features by autonomous community
+router.get('/region/:region', getFeaturesByComAut);
+
+// * Get features by country
+router.get('/country/:country', getFeaturesByCountry);
+
+// * Get features near a point
+router.get('/near/:lat/:long/:meters', getFeatureNearPoint)
+
+// * Get all the features
 router.get('/all/', getAllFeatures);
 
 
