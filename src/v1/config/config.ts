e833d1fab7334de5431fbc183e99f5823eddc204
xavigrausebastian <xavier.grau@kreatings.com>
2022-06-27 13:13:00 +0200

    Change config ts

--- src/v1/config/config.ts
+++ src/v1/config/config.ts
@@ -1,7 +1,7 @@
 export const config = {
 
     // Server port
-    PORT: 8000,
+    PORT: process.env.PORT || 8000,
 
     // Database connection uri
     URI: process.env.URI || "mongodb+srv://dumbledore:Jf8YGcCwrGnAExpZ@cluster0.kddpd.mongodb.net/pharmacies"
