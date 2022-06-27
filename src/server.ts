f8d1fcfc14a90aedac0ab04b0e5e601e19c2ea93
xavigrausebastian <xavier.grau@kreatings.com>
2022-06-27 10:58:53 +0200

    REMOVE unused imports

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 import express, { Request, Response} from 'express';
-export const app = express();
-const fileUpload = require('express-fileupload');
-const bodyParser = require("body-parser");
-const cors = require('cors');
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
-import expressLayouts from 'express-ejs-layouts';
+export const app = express();
+
 // * Connection file
 import { initDatabase } from "./connection";
 
@@ -13,16 +12,8 @@ import { config } from "./config/config";
 
 // * Import routes file
 import locations from './routes/locations';
-import uploads from './routes/upload';
 
 //EJS
-app.use(expressLayouts);
-app.set('view engine', 'ejs');
-app.use(fileUpload({
-    createParentPath: true
-}));
-app.use(express.static(__dirname + '/public'));
-
 app.use(bodyParser.urlencoded({
     extended: true
 }));
@@ -35,16 +26,11 @@ app.use(cors({
 app.get("/", (req: Request, res: Response) => {
     res.status(200).send("I'm working!");
 });
+
 // * Routes
 app.use("/city", locations);
 app.use("/all",locations);
-app.use("/",uploads);
 
-
-//Form
-app.get("/submit",function (req, res){
-    res.render('form.ejs')
-});
 
 // * Database and start server
 initDatabase(config.PORT, config.URI, app);
