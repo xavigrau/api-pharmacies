4753458456817106cc4b7a1d0e955c332c3e9fa7
xavigrausebastian <xavier.grau@kreatings.com>
2022-06-27 11:38:32 +0200

    REFACTOR middlewareSetup.ts

--- src/v1/services/middlewareSetup.ts
+++ src/v1/services/middlewareSetup.ts
@@ -1,16 +1,22 @@
 import bodyParser from "body-parser";
 import { app } from "../server";
 import cors from 'cors';
+import morgan from 'morgan';
 
 export default () => {
 
-    // * EJS
+    // * Body Parser
     app.use(bodyParser.urlencoded({
         extended: true
     }));
     app.use(bodyParser.json());
+
+    // * Cors
     app.use(cors({
         origin: 'http://localhost:8080'
     }));
+
+    // * Morgan
+    app.use(morgan('dev'));
 
 }
--- src/v1/services/routesSetup.ts
+++ src/v1/services/routesSetup.ts
@@ -1,13 +1,13 @@
 import { app } from "../server";
 
 // * Import routes file
-import locations from '../routes/features';
+import feature from '../routes/features';
 
 export default () => {
 
     // * Routes
-    app.use("/v1/city/", locations);
-    app.use("/v1/all/",locations);
+    app.use("/v1/city/", feature);
+    app.use("/v1/all/", feature);
 
 
 }
