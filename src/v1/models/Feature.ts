1536960d1fbc55eb86d60fd0c5bcc6ce05a8e6d2
xavigrausebastian <xavier.grau@kreatings.com>
2022-06-27 14:53:37 +0200

    MODIFY Feature schema

--- src/v1/models/Feature.ts
+++ src/v1/models/Feature.ts
@@ -1,9 +1,9 @@
 import mongoose, { Schema } from 'mongoose'
 
 const featureSchema = new Schema( {
-    type: String,
+    type: { type: String, required: true },
     geometry: {
-        type: String,
+        type: { type: String, required: true },
         coordinates: Array<number>
     },
     properties: {
@@ -17,36 +17,15 @@ const featureSchema = new Schema( {
         pharmacy_name: String,
         address: String,
         postalCode: String,
-        tel: String,
+        schedule: {
+            periods: Array,
+            weekday_text: Array<string>
+        },
+        formatted_phone_number: String,
         place_id: String,
-        schedule:String,
-        weekday_text:Array<string>
+        international_phone_number: String
     }
 })
-
-/*const FeatureSchema: Schema = new Schema({
-    type: { type: String, required: true },
-    geometry: {
-        type: { type: String, required: true },
-        coordinates: { type: Array, required: true }
-    },
-    properties: {
-        cnn: { type: String, required: true },
-        caac: { type: String, required: true },
-        country: { type: String, required: true },
-        comAut: { type: String, required: true },
-        province: { type: String, required: true },
-        town: { type: String, required: true },
-        name: { type: String, required: true },
-        pharmacy_name: { type: String, required: true },
-        postalCode: { type: String, required: true },
-        tel: { type: String, required: true },
-        place_id: { type: String, required: true },
-        address: { type: String, required: true },
-        schedule: { type: Array, required: true },
-        weekday_text: { type: String, required: true },
-    }
-})*/
 
 // * Export the model and return the interface
 export default mongoose.model('Feature', featureSchema);
